49980782d955af823bfc8a410bb8cb2b60928015
Piot Żurawski <pzurawski@soldevelo.com>
2016-04-15 13:52:27 +0200

    MOTECH-2168 Add selenium test for check API Documentation

--- framework/src/main/java/org/motechproject/uitest/page/RestApiPage.java
+++ framework/src/main/java/org/motechproject/uitest/page/RestApiPage.java
@@ -3,16 +3,10 @@ package org.motechproject.uitest.page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-/**
- * A class that represents data services page. Has methods which check functionality of
- * data browser, schema editor and data services settings.
- */
-
 public class RestApiPage extends MotechPage {
 
     private static final String HOME_PATH = "/module/server/home#";
     public static final By DATA_SERVICES = By.id("modulelink_rest-docs");
-    public static final By API_DOCUMENTATION = By.linkText("API Documentation");
     public static final By REST_API_DOCUMENTATION_LINK = By.linkText("REST API Documentation.");
 
 
