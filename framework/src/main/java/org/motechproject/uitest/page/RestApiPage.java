6f706ba1a11e662b66df7eb9ef5a82f5054c0579
Piot Żurawski <pzurawski@soldevelo.com>
2016-04-15 15:47:49 +0200

    Motech 2168 - Add selenium test for check REST API Documentation

--- framework/src/main/java/org/motechproject/uitest/page/RestApiPage.java
+++ framework/src/main/java/org/motechproject/uitest/page/RestApiPage.java
@@ -4,27 +4,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 /**
- * A class that represents data services page. Has methods which check functionality of
- * data browser, schema editor and data services settings.
+ * A class that represents REST API page. Has methods which check functionality of API Documentation.
  */
-
 public class RestApiPage extends MotechPage {
 
     private static final String HOME_PATH = "/module/server/home#";
-    public static final By DATA_SERVICES = By.id("modulelink_rest-docs");
+    public static final By MDS_REST_API_TITLE_PATH = By.xpath("//*[@id=\"api_info\"]/div[1]");
+    public static final By DATA_SERVICES_API_LINK = By.id("modulelink_rest-docs");
     public static final By REST_API_DOCUMENTATION_LINK = By.linkText("REST API Documentation.");
-
 
     public RestApiPage(WebDriver driver) {
         super(driver);
     }
 
-    public void openDataServics() throws InterruptedException {
-        clickWhenVisible(DATA_SERVICES);
+    public RestApiPage openDataServicsApi() throws InterruptedException {
+        clickWhenVisible(DATA_SERVICES_API_LINK);
+        return new RestApiPage(getDriver());
     }
 
     public String getDocumentationLink() throws InterruptedException {
         return findElement(REST_API_DOCUMENTATION_LINK).getAttribute("href");
+    }
+
+    public String getApiDocumentationTitle() throws InterruptedException {
+        return findElement(MDS_REST_API_TITLE_PATH).getText();
     }
 
     @Override
--- tests/src/test/java/org/motechproject/testing/uifunctionaltests/RestApiDocumentationUIFT.java
+++ tests/src/test/java/org/motechproject/testing/uifunctionaltests/RestApiDocumentationUIFT.java
@@ -9,11 +9,11 @@ import org.motechproject.uitest.page.RestApiPage;
 
 import static org.junit.Assert.assertEquals;
 
-
 public class RestApiDocumentationUIFT extends TestBase {
 
     private RestApiPage restApiPage;
     private static final String DOCUMENTATION_URL = "http://docs.motechproject.org/en/latest/get_started/model_data/model_data.html#the-rest-api";
+    private static final String MDS_REST_API_TITLE= "MDS REST API";
 
     @Before
     public void initialize() throws InterruptedException {
@@ -25,10 +25,11 @@ public class RestApiDocumentationUIFT extends TestBase {
     public void cleanUp() throws InterruptedException {
         logout();
     }
-
+    //MOTECH 2168
     @Test
-    public void openRestApiDocumentation() throws InterruptedException {
-        restApiPage.openDataServics();
+    public void shouldOpenRestApiDocumentation() throws InterruptedException {
+        restApiPage.openDataServicsApi();
+        assertEquals(MDS_REST_API_TITLE,restApiPage.getApiDocumentationTitle());
         assertEquals(DOCUMENTATION_URL,restApiPage.getDocumentationLink());
     }
 
